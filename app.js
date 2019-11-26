0e5066248f751c760fddc6fcf174ddda68a86510
enterdoki <chen.tommy925@gmail.com>
2019-11-25 22:48:44 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const app = express();
+const db = require('./database/db');
+const router = require('./routes/index');
+
+app.use(cors());
+app.use('/', router);
+
+db.sync({
+    force: false
+},
+    app.listen(port, () => {
+        console.log(`Server listening on port: ${port}`);
+    })
+)
+// app.get('/', (req, res, next) => {
+//     res.status(200).send('Hello World!');
+// })
+
---
+++ database/db.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const Sequelize = require('sequelize')
+const tunnel = require('tunnel-ssh');
+
+const db = new Sequelize(process.env.DATABASE_URL)
+
+// tunnel config 
+var config = {
+    username: process.env.SSH_USERNAME,
+    password: process.env.SSH_PASSWORD,
+    host: process.env.SSH_HOST,
+    port: 22,
+    dstPort: 3306,
+    keepAlive:true,
+    ssl:true 
+};
+
+tunnel(config, function (error, server) {
+    //....   
+    if (error) {
+        console.error(error);
+    } else {
+        // test sequelize connection     
+        db.authenticate().then(function (err) {
+            console.log('Connection established');
+        }).catch(function (err) {
+            console.error('Unable establish connection', err);
+        })
+        
+    }
+})
+
+module.exports = db;
+
